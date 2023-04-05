3ace2f3c33ec53f5fe54250581afc07f521ce7f1
millooo94 <camillomusmeciunict@gmail.com>
2023-04-05 17:26:57 +0200

    add all

---
+++ la-mia-pizzeria-static/Models/Pizza.cs
@@ -0,0 +1,19 @@
+﻿namespace la_mia_pizzeria_static.Models
+{
+    public class Pizza
+    {
+        public Pizza(string name, string description, float price, string img) 
+        { 
+            Name = name;
+            Description = description;
+            Price = price;
+            Img = img;
+        }
+
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public float Price { get; set; }
+        public string Img { get; set; }
+
+    }
+}
